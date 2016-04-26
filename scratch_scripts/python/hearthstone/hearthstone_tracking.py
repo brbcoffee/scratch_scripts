23057e5728f14a811f376c59a2d5ab915a5199fe
Eric Young <ericyoung@OEC00751-3.local>
2016-04-25 17:53:31 -0700

    working pre-comment removal

--- scratch_scripts/python/hearthstone/hearthstone_tracking.py
+++ scratch_scripts/python/hearthstone/hearthstone_tracking.py
@@ -4,11 +4,23 @@
 # It will eventually read from postgres to provide historical data during the game
 
 import json
+
+def write_to_json(data_chunk):
+    with open('data.json') as feedsjson:
+        feeds = json.load(feedsjson)
+    with open('data.json', 'w') as feedsjson:
+        entry = json.dumps(data_chunk)
+        feeds.append(entry)
+        json.dump(feeds, feedsjson)
+
 data = {}
 
 
 opponent_class = ""
 class_list = ['rogue', 'druid', 'mage', 'paladin','priest','warrior','warlock','hunter','shaman']
+
+with open('data.json', 'w') as f:
+    json.dump([], f)
 
 while opponent_class not in class_list:
     opponent_class = raw_input("What class is your opponent playing? ").lower()
@@ -21,16 +33,37 @@ game_on = 1;
 
 
 data["oppponentClass"] = opponent_class
-json_data = json.dumps(data)
-print json_data
-with open('data.json', 'w') as f:
-    json.dump(json_data,f)
+write_to_json(data)
 
-while game_on == 1:
+#json_data = json.dumps(data)
+
+
+#with open('data.json') as feedsjson:
+#    feeds = json.load(feedsjson)
+
+#with open('data.json', 'w') as feedsjson:
+#    entry = json.dumps(data)
+#    feeds.append(entry)
+#    print feeds
+#    json.dump(feeds, feedsjson)
+
+
+while game_on != "over":
     print "Turn %s" % turn
     opponents_play = raw_input("What has your opponent played on %s? " % turn)
     data["turn"] = turn
     data["opponentsPlay"] = opponents_play
-    json_data = json.dumps(data)
-    print json_data
 
+    write_to_json(data)
+
+#    with open('data.json', 'w') as feedsjson:
+#        entry = json.dumps(data)
+#        feeds = json.load(feedsjson)
+#        feeds.append(entry)
+#        json.dump(feeds, feedsjson)
+
+#    with open('data.json', 'w') as f:
+#        json.dump(json_data,f)
+    game_on = raw_input("type 'over' if the game ended").lower()
+    if raw_input("Did the opponent complete their turn? (y/n)").lower() == "y":
+        turn +=1
