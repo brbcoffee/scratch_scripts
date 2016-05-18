0d2cc953f0c086869a4c96398af82ead1abf5da1
Eric Young <ericyoung@OEC00751-3.local>
2016-05-18 09:39:42 -0700

    add bash loop for curl

---
+++ scratch_scripts/bash/curl_loop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+url=solr-vip-as.pal.att:8080/solr/publisher/select?
+#while true; do
+  for i in $(cat list.txt); do
+    content="$(curl -s "$url/$i")"
+    echo "$content" >> output.txt
+  done
+
+    
