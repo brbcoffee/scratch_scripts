97acea575c72923162e5c9562b1cb52691e9acaf
Eric Young <ericyoung@OEC00751-3.local>
2016-05-18 09:43:33 -0700

    tweak script

--- scratch_scripts/bash/curl_loop.sh
+++ scratch_scripts/bash/curl_loop.sh
@@ -3,7 +3,8 @@
 url=solr-vip-as.pal.att:8080/solr/publisher/select?
 #while true; do
   for i in $(cat list.txt); do
-    content="$(curl -s "$url/$i")"
+#   echo "$url$i"
+    content="$(curl -s "$url$i")"
     echo "$content" >> output.txt
   done
 
