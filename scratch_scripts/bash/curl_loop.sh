cb380f2ab61cdfaeb111271fd0ec6bfaa4cda8af
Eric Young <ericyoung@OEC00751-5.local>
2016-11-25 21:56:42 -0800

    new file

--- scratch_scripts/bash/curl_loop.sh
+++ scratch_scripts/bash/curl_loop.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 url=solr-vip-as.pal.att:8080/solr/publisher/select?
-#while true; do
+while true; do
   for i in $(cat list.txt); do
-#   echo "$url$i"
     content="$(curl -s "$url$i")"
     echo "$content" >> output.txt
   done
-
+done
     
