3f641afbe3f98fe2ebe0239b259d71536d185797
Eric Young <ericyoung@OEC00751-5.local>
2016-11-27 23:16:59 -0800

    tweaks

--- scratch_scripts/python/hearthstone/scripts/parseLog.py
+++ scratch_scripts/python/hearthstone/scripts/parseLog.py
@@ -34,15 +34,19 @@ def identifyPlayersAndOrder(log):
                 if "OPPOSING PLAY (Hero)" in line:
                     opponent_hero = hero
                     identifiedOpponent = 1
-                    if "player=1" in line:
-                        opponent_first = 1
                 if "FRIENDLY PLAY (Hero)" in line:
                      my_hero = hero
                      identifiedMe = 1
-                     if "player=1" in line:
-                         me_first = 1
     print "My opponent is: %s" % opponent_hero
     print "My hero is: %s" % my_hero
+    while me_first == 0 and opponent_first == 0:
+        for line in log:
+            if "The Coin" in line:
+                if "player=1" in line:
+                    opponent_first = 1
+                elif "player=2" in line:
+                    me_first = 1
+
     if opponent_first == 0:
         data['me_first'] = '1'
         print "I went first."
@@ -57,64 +61,60 @@ identifyPlayersAndOrder(log)
 
 def playAnalysis(log):
     global turn_number
-    turn_number = int(1)
+    turn_number = int(0)
     game_started = int(0)
     turn_counter = int(0)
     three_done = int(0)
     for line in log:
-#        if "tag=TURN value=" in line:
-#            try: 
-#                turn_number = int(re.search('tag=TURN value=([^,]+)', line).group(1)) / 2
-#            except:
-#                pass
         if "NUM_CARDS_DRAWN_THIS_TURN value=3" in line:
             if three_done == 0:
                 three_done = 1
             else:
                 game_started = 1
                 continue
-            print "A"
-            print turn_number
-            #if turn_number == 0:
-            #    turn_counter +=1
-            #print turn_counter
         elif game_started == 0:
             continue
 
         if "OPPOSING PLAY" in line:
             try:
-#                print line
                 opponents_play = re.search('name=(.+?) id=', line).group(1)
-                data['opponents_plays'][turn_number].append(opponents_play)
+                corrected_turn = turn_number + opponent_first
+                data['opponents_plays'][corrected_turn].append(opponents_play)
             except:
                 print "Error"
-                data['opponents_plays'][turn_number].append('Error')
-            print "Opponent played: %s on turn %s" % (opponents_play, turn_number)
+                data['opponents_plays'][corrected_turn].append('Error')
+            print "Opponent played: %s on turn %s" % (opponents_play, corrected_turn)
         if "FRIENDLY PLAY" in line:
             try:
-#                print line
                 my_play = re.search('name=(.+?) id=', line).group(1)
-                data['my_plays'][turn_number].append(my_play)
+                corrected_turn = turn_number + me_first
+                data['my_plays'][corrected_turn].append(my_play)
             except:
-                print "Error"
-                data['my_plays'][turn_number].append('Error')
-            print "I played: %s on turn %s" % (my_play, turn_number)
+#                print "Error"
+                data['my_plays'][corrected_turn].append('Error')
+            print "I played: %s on turn %s" % (my_play, corrected_turn)
         if "brbcoffee tag=PLAYSTATE value=WON" in line:
             win = 1
         if "brbcoffee tag=PLAYSTATE value=LOST" in line:
             win = 0
 
         if "NUM_CARDS_DRAWN_THIS_TURN value=1" in line and turn_counter == 0:
-            print "B"
-            print turn_number
-            print turn_counter
             turn_counter = 1
+           # print "_________________"
+           # print "1 drawn match so it is still"
+           # print "turn number: %s " %turn_number
+           # print "turn counter and turn counter becomes: %s" %turn_counter
+           # print line
+           # print "__end_______________"
         elif "NUM_CARDS_DRAWN_THIS_TURN value=1" in line and turn_counter == 1:
-            print "C"
-            print turn_number
-            print turn_counter
             turn_counter = 0
             turn_number += 1
+           # print "_________________"
+           # print "1 drawn match so it is now"
+           # print "turn number: %s " %turn_number
+          #  print "and turn counter becomes: %s" %turn_counter
+          #  print line
+          #  print "__end_______________"
     return(win)    
 
 did_i_win = playAnalysis(log)
@@ -128,7 +128,7 @@ else:
 
 json_file = re.search('logs_for_processing/scrubbed_log.([^,]+)',sys.argv[1]).group(1)
 print json_file
-with open('json_%s.data' % json_file, 'w') as f:
+with open('json_files/json_%s.data' % json_file, 'w') as f:
      json.dump(data, f)
 
 
