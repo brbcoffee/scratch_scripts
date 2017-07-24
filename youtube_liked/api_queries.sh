2d362310cb76f1a1e9f9f53d27fd334d551c7c7a
Eric Young <ericyoung@OEC00751-5.local>
2017-07-24 15:10:49 -0700

    update

--- youtube_liked/api_queries.sh
+++ youtube_liked/api_queries.sh
@@ -8,10 +8,9 @@ my_api='AIzaSyAOLj5NfPkmhf30gOtsGBYrgCbpRM36-44' #works
 #usernames who commented on one of killians vids:
 getUsernames_channel='UCjdQaSJCYS4o2eG93MvIwqg' #works #killian
 getUsernames_part="snippet,id"
-
-###curl "https://www.googleapis.com/youtube/v3/search?key=$my_api&channelId=$getUsernames_channel&part=$getUsernames_part&order=date&maxResults=20"
-
+curl "https://www.googleapis.com/youtube/v3/search?key=$my_api&channelId=$getUsernames_channel&part=$getUsernames_part&order=date&maxResults=20"
 #end don't touch
+
 
 
 
@@ -20,8 +19,7 @@ getUsernames_part="snippet,id"
 #playlist of one of those people
 playlist_list_id='LL4kfP9ugDYv_jkiW9poPgOw'
 playlist_part="snippet,contentDetails"
-
-curl "https://www.googleapis.com/youtube/v3/playlists?part=$playlist_part&key=$my_api&id=$playlist_list_id&order=date&maxResults=20"
+#curl "https://www.googleapis.com/youtube/v3/playlists?part=$playlist_part&key=$my_api&id=$playlist_list_id&order=date&maxResults=20"
 # end don't touch
 
 
