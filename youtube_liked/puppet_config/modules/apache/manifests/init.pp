98da588c8d8b13cc3f6c6c989700b8ce3fbfe138
Eric Young <ericyoung@OEC00751-5.local>
2017-08-15 13:28:58 -0700

    add yum class including nagios package install

--- youtube_liked/puppet_config/modules/apache/manifests/init.pp
+++ youtube_liked/puppet_config/modules/apache/manifests/init.pp
@@ -2,10 +2,14 @@ class apache (
     $apachename = $::apache::params::apachename,
 ) inherits apache::params {
 
-    notify {"in apache":}
+    notify {"in apache. $apachename":}
     package { 'httpd':
         name => $apachename,
+        ensure => 'installed',
+    }
+    service {'httpd':
         ensure => 'running',
-	}
+        require => Package['httpd']
+    }
 
 }
